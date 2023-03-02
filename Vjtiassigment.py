2152b6eb4341111e47df5cd72425570ac7749b77
HarshShah05 <105606474+HarshShah05@users.noreply.github.com>
2023-03-02 12:45:09 +0530

    added cahnges

--- Vjtiassigment.py
+++ Vjtiassigment.py
@@ -7,13 +7,15 @@ blank =np.ones((1000,1000,3),dtype='uint8')
 blank[:]=255,255,255
 # cv.imshow("Blank",blank)
 
-cv.rectangle(blank,(250,250),(0,0),((0,0,255)),thickness=-1)
+cv.rectangle(blank,(100,100),(0,0),((0,0,255)),thickness=-1)
+cv.rectangle(blank,(1000,0),(1000,100),((0,255,255)),thickness=-1)
 # cv.imshow("blank",blank)
 
 
 
-cv.putText(blank,"VJTI",(70,120),cv.FONT_HERSHEY_TRIPLEX,1.0,(255,255,255),thickness=1)
+#cv.putText(blank,"VJTI",(70,120),cv.FONT_HERSHEY_TRIPLEX,1.0,(255,255,255),thickness=1)
 cv.imshow("Text",blank)
+cv.imwrite('bl.jpg',blank)
 cv.waitKey(0)
 
 
