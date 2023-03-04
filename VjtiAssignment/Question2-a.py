591272c052a97fc8458b31c3d1e7a46409f5cb20
HarshShah05 <105606474+HarshShah05@users.noreply.github.com>
2023-03-04 09:01:35 +0530

    changes to box

--- VjtiAssignment/Question2-a.py
+++ VjtiAssignment/Question2-a.py
@@ -61,15 +61,57 @@ elif str==2:
 elif str==3:
     a1=int(input("Enter first dimension: "))
     b1=int(input("Enter second dimension: "))
-    c1=int(input("Enter no of boxes:"))
+    color=[]
     blank=np.ones((b1,a1,3),dtype="uint8")
     blank[:]=255,255,255
+    d1=int(input("Enter number of boxes :"))
+    for i in range(1,d1+1):
+      ci=input(f"Enter color of box {i} in lowercase ")
+      color.append(ci)
+    if color[0]=="red":
+      c1=(0,0,255)
+    elif color[0]=="green":
+      c1=(0,255,0)
+    elif color[0]=="yellow":
+      c1=(0,255,255)
+    else:
+      c1=(255,0,0)
+      
+    # 2nd box
+    if color[1]=="red":
+            c2=(0,0,255)
+    elif color[1]=="green":
+          c2=(0,255,0)
+    elif color[1]=="yellow":
+          c1=(0,255,255)
+    else:
+          c2=(255,0,0)
+      
+    # 3rd box
+    if color[2]=="red":
+        c3=(0,0,255)
+    elif color[2]=="green":
+      c3=(0,255,0)
+    elif color[2]=="yellow":
+      c3=(0,255,255)
+    else:
+          c3=(255,0,0)
+      
+    # 4th box 
+    if color[3]=="red":
+          c4=(0,0,255)
+    elif color[3]=="green":
+        c4=(0,255,0)
+    elif color[3]=="yellow":
+       c4=(0,255,255)
+    else:
+       c4=(255,0,0)
+    
     def option3(blank):
-        
-      cv.rectangle(blank,(0,0),(a1//2,b1//2),(red),thickness=-1)
-      cv.rectangle(blank,(a1//2,0),(a1,b1//2),(green),thickness=-1)
-      cv.rectangle(blank,(0,b1//2),(a1//2,b1),(blue),thickness=-1)
-      cv.rectangle(blank,(a1//2,b1//2),(a1,b1),(yellow),thickness=-1)
+      cv.rectangle(blank,(0,0),(a1//2,b1//2),(c1),thickness=-1)
+      cv.rectangle(blank,(a1//2,0),(a1,b1//2),(c2),thickness=-1)
+      cv.rectangle(blank,(0,b1//2),(a1//2,b1),(c3),thickness=-1)
+      cv.rectangle(blank,(a1//2,b1//2),(a1,b1),(c4),thickness=-1)
       return (blank)
     img3=option3(blank)
     cv.imshow("Option 3",img3)
@@ -79,3 +121,8 @@ elif str==3:
 else:
   print("Plz enter among the 3 options only")
 
+red=(0,0,255)
+green=(0,255,0)
+blue=(255,0,0)
+yellow=(0,255,255)
+black=(0,0,0)
